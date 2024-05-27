7f82ce62a7be5e438e1ad5f17ccfb396c442c785
Jake Faulkner <jake.faulkner@canterbury.ac.nz>
2024-05-28 11:53:45 +1200

    add missing files

---
+++ setup.py
@@ -0,0 +1,20 @@
+from Cython.Build import cythonize
+from setuptools import Extension, setup
+
+# Define Cython extension module
+extensions = [Extension("merge_ts.merge_ts_loop", ["merge_ts/merge_ts_loop.pyx"])]
+
+setup(
+    name="merge_ts",
+    version="1.0",
+    packages=["merge_ts"],
+    ext_modules=cythonize(extensions),
+    install_requires=[
+        "cython",
+        "typer",
+        "qcore @ git+https://github.com/ucgmsim/qcore",
+        "setuptools",
+        "typer",
+    ],
+    entry_points={"console_scripts": ["merge_ts=merge_ts.merge_ts:main"]},
+)
